347b3123daa33e87bbf671c49ac53b8958c21860
Alexandra Paxton <paxton.alexandra@gmail.com>
2016-07-25 15:20:42 -0700

    Add stimulus list generation

--- experiment.py
+++ experiment.py
@@ -27,6 +27,22 @@ class FunctionLearning(Experiment):
         """Create a new node."""
         return Indexed(participant=participant, network=network)
 
+    def setup(self):
+        """Create the networks and add a source if the networks don't already exist."""
+        if not self.networks():
+            for _ in range(self.practice_repeats):
+                network = self.create_network()
+                network.role = "practice"
+                self.session.add(network)
+                ListSource(network=network)
+            for _ in range(self.experiment_repeats):
+                network = self.create_network()
+                network.role = "experiment"
+                self.session.add(network)
+                ListSource(network=network)
+            self.session.commit()
+        
+
 
 class Paired(Network):
     """Node <-> Node; Node <-> Node; ...
@@ -46,6 +62,18 @@ class Paired(Network):
         try:
             partner_node = Indexed.query.filter_by(network_id=node.network_id, index=partner_n_index).one()
             node.connect(direction="both", whom=partner_node)
+            
+            # grab the source created for the network
+            source = self.nodes(type=ListSource)[0]
+            
+            # connect the sources to both nodes and sends them both the same list
+            source.connect(whom=[node, partner_node])
+            source.transmit(what=source.new_list(), to_whom=[node, partner_node])
+            
+            # let both nodes receive the list that've been sent
+            node.receive()
+            partner_node.receive()
+
         except:
             pass
 
@@ -75,4 +103,36 @@ class Indexed(Node):
 
         super(Indexed, self).__init__(network, participant)
 
-        self.index = self.network.size()
+        self.index = self.network.size(type=Indexed)
+
+class ListSource(Source):
+    """A source that generates lists of numbers randomly sampled from a uniform distribution for 
+    each pair in a paired network. These lists are then sent to each pair."""
+
+    __mapper_args__ = {"polymorphic_identity": "listsource"}
+
+    def new_list(self):
+        """Generate a list of numbers randomly sampled from a uniform distribution."""
+
+        # import json library so that we can save our results as a string for transmission
+        import json
+
+        # create list container and specify number of trials
+        list_to_add = []
+        list_length = 20
+
+        # iterate over our desired number of trials to make a new list of the appropriate length
+        for new_item in range(list_length):
+            list_to_add.append(randint(0,100))
+
+        # ship our list as a string (which we'll then reconstitute as a list upon reading)
+        return Info(origin=self, contents=json.dumps(list_to_add))
+
+
+
+
+
+
+
+
+
