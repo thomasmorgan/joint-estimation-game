18004c074ae76a3b815292e6d17e65bac1c8d393
Alexandra Paxton <paxton.alexandra@gmail.com>
2018-01-25 22:46:29 -0800

    Use old version of 'questionnaire.js'

--- static/scripts/questionnaire.js
+++ static/scripts/questionnaire.js
@@ -1,76 +1,49 @@
 lock = false;
 
-// // Add new (not yet released) code from Dallinger.
-// dallinger.submitQuestionnaire = function (name) {
-//   var formSerialized = $("form").serializeArray(),
-//       formDict = {},
-//       deferred = $.Deferred();
-//
-//   formSerialized.forEach(function (field) {
-//       formDict[field.name] = field.value;
-//   });
-//
-//   reqwest({
-//     method: "post",
-//     url: "/question/" + dallinger.identity.participantId,
-//     data: {
-//       question: name || "questionnaire",
-//       number: 1,
-//       response: JSON.stringify(formDict),
-//     },
-//     type: "json",
-//     success: function (resp) {
-//       deferred.resolve();
-//     },
-//     error: function (err) {
-//       deferred.reject();
-//       var errorResponse = JSON.parse(err.response);
-//       $("body").html(errorResponse.html);
-//     }
-//   });
-//
-//   return deferred;
-// };
+// Add new (not yet released) code from Dallinger.
+Dallinger.submitQuestionnaire = function (name) {
+  var formSerialized = $("form").serializeArray(),
+      formDict = {},
+      deferred = $.Deferred();
 
+  formSerialized.forEach(function (field) {
+      formDict[field.name] = field.value;
+  });
+
+  reqwest({
+    method: "post",
+    url: "/question/" + participantId,
+    data: {
+      question: name || "questionnaire",
+      number: 1,
+      response: JSON.stringify(formDict),
+    },
+    type: "json",
+    success: function (resp) {
+      deferred.resolve();
+    },
+    error: function (err) {
+      deferred.reject();
+      var errorResponse = JSON.parse(err.response);
+      $("body").html(errorResponse.html);
+    }
+  });
+
+  return deferred;
+};
+
+// Cribbed from Dallinger Griduniverse repo:
+// https://github.com/Dallinger/Griduniverse/blob/master/dlgr/griduniverse/static/scripts/questionnaire.js
 $(document).ready( function() {
-
-  // // Submit the questionnaire ONLY if we haven't clicked yet.
-  // if (lock===false){
-  //     $("#submit-questionnaire").click(function() {
-  //         // Prevent multiple submission clicks.
-  //         lock = true;
-  //         $(document).off('click');
-  //
-  //         // submit questionnaire
-  //         dallinger.submitQuestionnaire("questionnaire");
-  //       });
 
   // Submit the questionnaire.
   $("#submit-questionnaire").click(function() {
-    console.log("Submitting questionnaire.");
     var $elements = [$("form :input"), $(this)],
-        questionSubmission = dallinger.submitQuestionnaire("questionnaire");
-
-    spinner.freeze($elements);
-    questionSubmission.done(dallinger.submitAssignment);
-    questionSubmission.always(function () {
-      spinner.unfreeze();
-    });
-
-
-      // $("#submit-questionnaire").click(function() {
-      //
-
-      //
-      //   // Allow the form to submit.
-      //   var $elements = [$("form :input"), $(this)],
-      //       questionSubmission = dallinger.submitQuestionnaire("questionnaire");
-      //       console.log("Submitting questionnaire.");
-
-        // // Submit questionnaire.
-        // questionSubmission.done(function()
-        //     {
-        //       go_to_page('debriefing');
-        //     });
+        questionSubmission = Dallinger.submitQuestionnaire("questionnaire");
+        console.log("Submitting questionnaire.");
+    questionSubmission.done(function()
+        {
+          go_to_page('debriefing');
+        });
   });
 });
